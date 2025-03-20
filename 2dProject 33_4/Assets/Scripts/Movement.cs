cede13ce3d5fa3916cf925412ea2d08590f5c1c5
Lasha Surguladze <44227081+surlabo@users.noreply.github.com>
2025-03-20 03:35:56 -0700

    init

---
+++ 2dProject 33_4/Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float jumpForce = 300f;
+    [SerializeField]
+    private float detectDisance = 1f;
+
+    private LayerMask layer = 1 << 3 | 1 << 6 ;
+
+    private Rigidbody2D rb;
+    private bool canJump;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        float dir = Input.GetAxis("Horizontal");
+        Vector2 vectorDir = new Vector2(dir, 0);
+        transform.Translate(vectorDir * Time.deltaTime * speed);
+
+        var hit = Physics2D.Raycast(transform.position, Vector2.down, detectDisance, 1 << 3);
+        if (hit.collider != null)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        
+        
+        Debug.DrawRay(transform.position, Vector2.down * detectDisance, Color.red);
+    }
+}
